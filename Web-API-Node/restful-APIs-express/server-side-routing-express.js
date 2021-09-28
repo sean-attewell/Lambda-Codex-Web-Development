702da9859a4f8477e99db6d5b8fea7a9e47d1569
sean-attewell <sean.attewell@gmail.com>
2021-09-28 16:19:53 +0100

    express router setup variations

--- Web-API-Node/restful-APIs-express/server-side-routing-express.js
+++ Web-API-Node/restful-APIs-express/server-side-routing-express.js
@@ -200,3 +200,45 @@ server.put('/hobbits/:id', (req, res) => {
 // Concentrate on the code related to reading the id from the req.params object and reading the hobbit information from req.body. 
 // The rest of the code will change as this is a simple example using an in-memory array. Most production APIs will use a database.
 
+
+// What is REST?
+// A way of designing and distributing software
+// Principles introduced by Roy Fielding in 1999, not a standard
+
+// REST is a generally agreed-upon set of principles and constraints. They are recommendations, not a standard.
+// When designing a RESTful Web API, keep the following principles in mind:
+
+// Everything is a resource.
+// Each resource is accessible via a unique URI.
+// Resources can have multiple representations.
+// Communication happens over a stateless protocol (HTTP).
+// Resource management happens via HTTP methods.
+
+// Applying the REST architecture to our APIs can make them scalable and simpler to maintain and extend.
+
+// REST APIs have six constraints:
+
+// client-server architecture.
+// stateless architecture: each request should stand on its own, and order should not matter. No shared state.
+// cacheable: improves network performance.
+//  GET, PUT, and DELETE should be idempotent 
+//  (the same command executed multiple times, the state of resources on the server is exactly the same, much like pure functions)
+//  POST is not idempotent.
+//  Caching is a way to store and retrieve data so that the server can fulfill future requests faster without repeating expensive calculations or operations.
+
+// layered system: component A (a client) might or might not communicate directly with component B (the server). 
+// There may be other layers between them like logging, caching, DNS servers, load balancers, and authentication.
+// code on demand
+//   The API returns the resource and code to act on it.
+//   The client only needs to know how to execute the code.
+//   Makes the API more flexible, upgradeable, and extendible.
+//   Most web applications send JavaScript code along with the data.
+
+// uniform interfaces
+// Each resource should be accessible through a single URL. Not a hard requirement, but recommended.
+// We should be able to manage the resources through these representations (the URL).
+// every interaction with the resource should happen through the URL identifier we gave to it.
+// Self-descriptive messages.
+// HATEOAS (Hypermedia As The Engine Of Application State). Much like a choose your own adventure book, the pages are not read in order. 
+// You start on page 1. Based on the information available, the reader (client) chooses the action to take, moving them to a different page. 
+// A good example of a hypermedia API is the GitHub API.
